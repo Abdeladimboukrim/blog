b1237db91e60f77a5eaf965da800c26e1afc02d2
abdeladimboukrim <boukrimabdeladim@email.com>
2021-05-06 11:10:56 +0000

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+//app link
+
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'https://localhost/blog/');
+define('APPNAME', 'abdeladim');
+
+
+
+//create database info
+define("DB_HOST", "localhost");
+define("DB_USER", 'root');
+define("DB_PASSWORD", '');
+define("DB_NAME", 'blog');
