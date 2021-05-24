15306c05be0a671b42348550cafb69c0e42396ca
abdeladimboukrim <boukrimabdeladim@email.com>
2021-05-24 15:57:39 +0100

    Create index.php

---
+++ app/views/pages/index.php
@@ -0,0 +1,11 @@
+<?php include_once APPROOT . '/views/inc/header.inc.php'; ?>
+<div class="card mb-3">
+    <img src="./public/img/diapo.png" class="d-block user-select-none" width="100%" height="700" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
+    <rect width="100%" height="100%" fill="#868e96"></rect>
+    
+  </svg>
+    
+   
+ 
+</div>
+ <?php include_once APPROOT . '/views/inc/footer.inc.php'; ?>
