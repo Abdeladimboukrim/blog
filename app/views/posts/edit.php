96a40956d862737a96becd0d4b76a64eb0ad0a79
abdeladimboukrim <boukrimabdeladim@email.com>
2021-05-24 16:07:30 +0100

    Create edit.php

---
+++ app/views/posts/edit.php
@@ -0,0 +1,22 @@
+<?php include_once APPROOT . '/views/inc/header.inc.php'; ?>
+<!-- <pre><?php var_dump($data);  ?></pre> -->
+<div class="card card-body mt-5">
+    <h2 class="mb-3">Create new Post</h2>
+    <form action="<?php echo URLROOT; ?>posts/edit/<?php echo $data['postId'] ?>" method="post">
+        <div class="form-group">
+            <input type="text" name="title" value="<?php echo $data['title']; ?>" placeholder="title ..." class="form-control form-control-lg <?php echo (!empty($data['title_err']) ? 'is-invalid' : '') ?>">
+            <span class="invalid-feedback"> <?php echo $data['title_err'] ?></span>
+        </div>
+        <div class="form-group">
+            <textarea name="body" placeholder="post body ..." class="form-control form-control-lg <?php echo (!empty($data['body_err']) ? 'is-invalid' : '') ?>"><?php echo $data['body']; ?></textarea>
+            <span class="invalid-feedback"> <?php echo $data['body_err'] ?></span>
+        </div>
+        <input type="text" value="<?php echo $data['postId'] ?>"  name="postId" hidden >
+        <div>
+       
+            <input type="submit" class="btn btn-success">
+        </div>
+    </form>
+</div>
+
+<?php include_once APPROOT . '/views/inc/footer.inc.php'; ?>
